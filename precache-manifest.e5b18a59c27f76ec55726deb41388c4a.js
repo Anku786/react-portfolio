5551ed7725b806228deb1b670bdaf30f4a95ad60
Anku786 <bishnoiankita23@gmail.com>
2020-08-11 22:04:44 -0700

    Updates

---
+++ precache-manifest.e5b18a59c27f76ec55726deb41388c4a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fe8a6f6f1ba6876c4440027646d7e6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "bd4104c74d182fe0d313",
+    "url": "/react-portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "64b124fcf987cdbbce6b",
+    "url": "/react-portfolio/static/css/main.163dfd1c.chunk.css"
+  },
+  {
+    "revision": "bd4104c74d182fe0d313",
+    "url": "/react-portfolio/static/js/2.535fbcfe.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/react-portfolio/static/js/2.535fbcfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b124fcf987cdbbce6b",
+    "url": "/react-portfolio/static/js/main.a9583603.chunk.js"
+  },
+  {
+    "revision": "856d3abcd398afe2b77c",
+    "url": "/react-portfolio/static/js/runtime-main.978b8f8e.js"
+  },
+  {
+    "revision": "21c71d71629d7f44d104a6959bb81452",
+    "url": "/react-portfolio/static/media/AnkitaCV.21c71d71.pdf"
+  },
+  {
+    "revision": "1b8beb3f289d35e02d4f221aeba2a7c9",
+    "url": "/react-portfolio/static/media/Basic.1b8beb3f.pdf"
+  },
+  {
+    "revision": "c5cd362fdf18289aefef7675a8ea846b",
+    "url": "/react-portfolio/static/media/Carousel.c5cd362f.jpg"
+  },
+  {
+    "revision": "4ba6ee05d8ab5bdcaac36517e9225983",
+    "url": "/react-portfolio/static/media/Javascript.4ba6ee05.pdf"
+  },
+  {
+    "revision": "0447cb54074609881a13f23ef377d348",
+    "url": "/react-portfolio/static/media/Mern.0447cb54.pdf"
+  },
+  {
+    "revision": "83e956f428e97dcd05848232c32ac854",
+    "url": "/react-portfolio/static/media/Python.83e956f4.pdf"
+  },
+  {
+    "revision": "9f914c55d264bcff83100789b86b301c",
+    "url": "/react-portfolio/static/media/Server.9f914c55.pdf"
+  },
+  {
+    "revision": "7bcc0ee51677b691cb1f76ea71dec6aa",
+    "url": "/react-portfolio/static/media/Upgrad.7bcc0ee5.pdf"
+  },
+  {
+    "revision": "ec7cbd23bd79e824ba5f661009ec30bf",
+    "url": "/react-portfolio/static/media/background.ec7cbd23.jpg"
+  },
+  {
+    "revision": "2ebd858a1dc9f84c66600b37f4c18362",
+    "url": "/react-portfolio/static/media/image.2ebd858a.jpg"
+  }
+]);
